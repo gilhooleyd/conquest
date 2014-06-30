de6f985c04d873135bf62f89564b4b3852970672
David Gilhooley <djg5@princeton.edu>
2014-06-30 10:14:18 -0400

    Did some things

--- html5/js/draw.js
+++ html5/js/draw.js
@@ -6,12 +6,12 @@ y_loc = 0;
 function drawGrid() 
 {
 	noise.seed(0);
-
+    
   var canvas = document.getElementById('canvas');
   if (canvas.getContext) 
   {
   	var ctx = canvas.getContext('2d');
-
+    ctx.fillStyle = 'rgb(0,0,0)';
     var w = canvas.width ;
     var h = canvas.height;
 
@@ -22,14 +22,14 @@ function drawGrid()
     offset = 3;
     var res = 1;
 
-    noise.setLayers(10, .4, .003);
+    noise.setLayers(10, .4, .07);
 
     w = w - w % (size + offset);
     h = h - h % (size + offset);
 
-    for (var x = 0; x < w; x += size + offset)
+    for (var x = 0; x * (size + offset) < w; x++)
     {
-    	for (var y = 0; y < h; y += size + offset)
+    	for (var y = 0; y * (size + offset) < h; y++)
     	{
     		var val = (noise.layerSimplex((x_loc + x) * res, (y_loc + y) * res) + 1) / 2 * 255;
     		val = parseInt(val);
@@ -37,7 +37,7 @@ function drawGrid()
     		ctx.fillStyle = 'rgb(' +val+', '+val+','+ val +')';
     		// ctx.strokeStyle = 'rgb(100,100,100)';
     		// ctx.fillStyle = 'green';
-    		ctx.fillRect(x , y, size, size);
+    		ctx.fillRect(x * (size + offset) , y * (size + offset), size, size);
     	}
     }
 
--- html5/js/keybindings.js
+++ html5/js/keybindings.js
@@ -8,10 +8,10 @@ function keyDown(e)
 {
 	var c = e.keyCode;
 
-	if (c == 119) y_loc++;
+	if (c == 119) y_loc--;
 	else if (c == 97) x_loc--;
-	else if (c == 115) y_loc--;
-	else if (c == 100) y_loc--;
+	else if (c == 115) y_loc++;
+	else if (c == 100) x_loc++;
 	
 	drawGrid();
 }
